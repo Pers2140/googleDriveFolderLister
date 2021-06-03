dab3fadcc60dedbb0d80bddccfbbcde457850aa2
Pers2140 <Pers2140@fredonia.edu>
2021-06-03 18:51:53 +0000

    user can run more than once

--- __main__.py
+++ __main__.py
@@ -6,25 +6,34 @@ import googleDriveAPI
 # Check __name__ == "__main__.py ==> run main()"
 
 if __name__ == "__main__":
+    
+    ans = True
+    while ans:
+        try:
+            # exampel folder id "0B69RRb17QL4pflctSUpZYmlScVlMNmFKVnFuS1RLT1BHR1BGZkR2eW5YYmhiQlZMSmo2Nm8"
+            # Specify the folder 
+            # Get user input
+            print ('\nEnter folder id: \n')
+            folder_id = input()
+            res = driveCaller(folder_id)
+            print ('\nYour output =>\n')
+            print (res)
+            print ('\n ... sending output to file\n\n')
 
-    try:
-        # exampel folder id "0B69RRb17QL4pflctSUpZYmlScVlMNmFKVnFuS1RLT1BHR1BGZkR2eW5YYmhiQlZMSmo2Nm8"
-        # Specify the folder 
-        # Get user input
-        print ('\nEnter folder id: \n')
-        folder_id = input()
-        res = driveCaller(folder_id)
-        print ('\nYour output =>\n')
-        print (res)
-        print ('\n ... sending output to file\n\n')
+            # write contents to file 
+            f = open('filecontents.txt','w')
+            f.write(str(res))
+            f.close()
 
-        # write contents to file 
-        f = open('filecontents.txt','w')
-        f.write(str(res))
-        f.close()
-    except:
+            #check if user wants to run again
+            print ('Would you like to run again?')
+            run_again = input()
+            if run_again != 'yes':
+                ans = False
 
-        print ('error has occured')
+        except:
+            ans = False
+            print ('error has occured')
 
 
 
